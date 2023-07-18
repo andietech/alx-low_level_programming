4c5fe3a9e89ffbef3f4e040b00dfde32177a82d4
andietech <andolederick@gmail.com>
2023-07-18 16:53:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase  = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if (lowercase != 'q' && lowercase != 'e')
+			putchar(lowercase);
+	}
+	putchar('\n');
+	return (0);
+}
