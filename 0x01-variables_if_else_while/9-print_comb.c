bbb6fe00a361f575bddbca6aca5668fcb29c7197
andietech <andolederick@gmail.com>
2023-07-19 05:46:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 when on success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar('0' + number);
+		if (number != 9)
+		}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
