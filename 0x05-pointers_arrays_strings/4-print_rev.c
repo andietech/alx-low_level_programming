84d3d94c462a1088fc8a6b89407e56dcbec083ca
andietech <andolederick@gmail.com>
2023-07-25 14:57:07 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s:The string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s
